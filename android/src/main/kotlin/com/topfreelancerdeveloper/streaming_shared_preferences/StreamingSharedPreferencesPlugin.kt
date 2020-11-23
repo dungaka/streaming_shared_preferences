52657bc936e035683e851cfa151789de10af64c2
Ali Abdollahi <abdollahi890@gmail.com>
2020-11-23 13:47:26 +0330

    Finish Feature-android

--- android/src/main/kotlin/com/topfreelancerdeveloper/streaming_shared_preferences/StreamingSharedPreferencesPlugin.kt
+++ android/src/main/kotlin/com/topfreelancerdeveloper/streaming_shared_preferences/StreamingSharedPreferencesPlugin.kt
@@ -1,21 +1,26 @@
 package com.topfreelancerdeveloper.streaming_shared_preferences
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+
 
 /** StreamingSharedPreferencesPlugin */
-class StreamingSharedPreferencesPlugin: FlutterPlugin, MethodCallHandler {
+class StreamingSharedPreferencesPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var activity : Activity;
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "streaming_shared_preferences")
@@ -23,14 +28,39 @@ class StreamingSharedPreferencesPlugin: FlutterPlugin, MethodCallHandler {
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    if (call.method == "getValue") {
+      val key: String? = call.argument("key");
+      val name : String?= call.argument("name");
+      if(name == null)
+      result.success(activity.getPreferences(Context.MODE_PRIVATE).getString(key, null));
+      else
+        result.success(activity.getSharedPreferences(name , Context.MODE_PRIVATE).getString(key, null));
+    }
+    if (call.method == "setValue") {
+      val key: String? = call.argument("key");
+      val value : String?= call.argument("value");
+      val name : String?= call.argument("name");
+      if(name == null)
+      result.success(activity.getPreferences(Context.MODE_PRIVATE).edit().putString(key , value).commit());
+      else result.success(activity.getSharedPreferences(name , Context.MODE_PRIVATE).edit().putString(key , value).commit());
+
     }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    this.activity = binding.activity;
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+  }
+
+  override fun onDetachedFromActivity() {
   }
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -14,31 +16,25 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
+  String value = 'default';
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await StreamingSharedPreferences.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+    StreamingSharedPreferences.addObserver('temp', (value) {
+      print('UPDATEEEEEE');
+      setState(() {
+        this.value = value;
+      });
+    });
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+    Future.doWhile(() async {
+      await Future.delayed(Duration(seconds: 1));
+      StreamingSharedPreferences.setValue(
+          'temp', Random().nextInt(100).toString());
+      print('SETTTTTTTTTT');
 
-    setState(() {
-      _platformVersion = platformVersion;
+      return true;
     });
   }
 
@@ -47,10 +43,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          title: Text('$value'),
         ),
       ),
     );
